0558fe3070e3abe35df12cbce7552656d33e25bf
PahanZ <zagvzdin.pakhan.webdev@gmail.com>
2026-04-05 19:24:35 +0200

    rabit

---
+++ backend/internal/queue/rabbitmq.go
@@ -0,0 +1,54 @@
+package queue
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/pahan-fe/lite-streaming/backend/internal/config"
+	"context"
+)
+
+type RabbitMQ struct {
+	channel *amqp.Channel
+}
+
+func (r *RabbitMQ) Publish(queueName string, body []byte) error {
+	_, err := r.channel.QueueDeclare(queueName, true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	err = r.channel.PublishWithContext(context.Background(), "", queueName, false, false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body: body,
+		})
+
+	return err
+}
+
+func (r *RabbitMQ) Consume(queueName string) (<-chan amqp.Delivery, error) {
+	_, err := r.channel.QueueDeclare(queueName, true, false, false, false, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	messages, err := r.channel.Consume(queueName, "", true, false, false, false, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return messages, nil
+}
+
+func NewRabbitMQ(c *config.Config) (*RabbitMQ, error) {
+	conn, err := amqp.Dial(c.RabbitMQURL)
+	if err != nil {
+		return nil, err
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+
+	return &RabbitMQ{channel: ch}, nil
+}
