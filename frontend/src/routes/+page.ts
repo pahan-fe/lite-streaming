bdbef474d52081ea5ba4bd2889c609133d10aa89
PahanZ <zagvzdin.pakhan.webdev@gmail.com>
2026-05-16 21:57:48 +0200

    drop svelte

--- frontend/src/routes/+page.ts
+++
@@ -1,9 +0,0 @@
-import { fetchVideoList } from '$lib/features/video/api';
-import { pageSize } from '$lib/features/video/const';
-import type { PageLoad } from './$types'; 
-
-export const load: PageLoad = async ({ fetch }) => {
-    const videos = await fetchVideoList(fetch, 1, pageSize);
-
-    return { videos };
-}
